8952aef6b7db5d815661e8407f0eeff1d398e178
Joe Revangile <jrevangile@mail.valenciacollege.edu>
2016-02-17 12:30:18 -0500

    reverted changes of name change
    I missed where is said to edit line 4 -- recommitting.

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is '
+	return 'hello world, my name is ' joe
 
-print( hello_world() + 'joe' )
+print( hello_world() + 'hector' )
